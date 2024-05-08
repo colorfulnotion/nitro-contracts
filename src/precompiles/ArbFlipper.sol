0db3ee257c8d68e7d92dd340c002d0cf4cd718c2
Sourabh Niyogi <sourabh@colorfulnotion.com>
2024-05-08 22:00:28 +0000

    ArbFlipper

---
+++ src/precompiles/ArbFlipper.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.4.21 <0.9.0;
+
+interface ArbFlipper {
+    // Emit event when Flip is called
+    event Flipped(address indexed caller);
+
+    event Hi(address indexed caller);
+
+    function flip() external returns (string memory);
+
+    function getNumber() external view returns (uint64);
+
+    function setNumber(uint64 newNumber) external returns (bool);
+
+    function getFlipState() external view returns (uint64);
+
+    function getFlipCount() external view returns (uint64);
+}
